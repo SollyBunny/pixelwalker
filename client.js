207097392179ab02cd05cfc428d8bc9cf11a0cfa
SollyBunny <sollybunn@gmail.com>
2024-12-25 09:51:17 +0000

    Remove async from constructors

--- client.js
+++ client.js
@@ -14,26 +14,43 @@ function int32ToRgba(int32) {
 }
 
 export class Client {
-	static async fromLogin(identity, password, local) {
+	constructor(local) {
+		this.local = Boolean(local);
+		this.pocketbase = new PocketBase(URLServerAPI(this.local));
+	}
+	async init() {
+		await Promise.all([
+			(async () => {
+				// Get room types
+				this.roomTypes = await fetch(`${URLServerGame(this.local)}/listroomtypes`);
+				this.roomTypes = await this.roomTypes.json();
+			})(),
+			(async () => {
+				// Get mappings
+				this.blockIds = await fetch(`${URLServerGame(this.local)}/mappings`);
+				this.blockIds = await this.blockIds.json();
+				// Create block manager
+				this.blockManager = new BlockManager(this, this.blockIds);
+				// Get color map
+				this.blockColors = (await import("./dataBlockColors.js")).default;
+				this.blockColors = Object.fromEntries(Object.entries(this.blockColors).map(([name, color]) =>
+					[this.blockManager.name(Number(name)), int32ToRgba(color)]
+				));
+			})()
+		]);
+	}
+	async authWithCredentials(identity, password) {
 		if (!identity)
 			throw new Error("Identity cannot be empty");
 		if (!password)
 			throw new Error("Password cannot be empty");
-		const client = new Client(local);
 		// may throw, TODO: clean up error message
-		await client.pocketbase.collection("users").authWithPassword(identity, password);
-		return client;
+		await this.pocketbase.collection("users").authWithPassword(identity, password);
 	}
-	static fromToken(token, local) {
+	async authWithToken(token) {
 		if (!token)
 			throw new Error("Token cannot be empty");
-		const client = new Client(local);
-		client.pocketbase.authStore.save(token, { verified: true });
-		return client;
-	}
-	constructor(local) {
-		this.local = Boolean(local);
-		this.pocketbase = new PocketBase(URLServerAPI(this.local));
+		this.pocketbase.authStore.save(token, { verified: true });
 	}
 	get token() {
 		const token = this.pocketbase.authStore.token;
@@ -49,35 +66,6 @@ export class Client {
 		if (!id)
 			throw "Invalid token";
 		return id;
-	}
-	async roomTypes() {
-		if (!this._roomTypes) {
-			this._roomTypes = await fetch(`${URLServerGame(this.local)}/listroomtypes`);
-			this._roomTypes = await this._roomTypes.json();
-		}
-		return this._roomTypes;
-	}
-	async blockColorsMap() {
-		if (!this._blockColorsMap) {
-			const blockManager = await this.blockManager();
-			this._blockColorsMap = (await import("./blockColorsMap.js")).default;
-			this._blockColorsMap = Object.fromEntries(Object.entries(this._blockColorsMap).map(([name, color]) => {
-				return [blockManager.name(Number(name)), int32ToRgba(color)];
-			}));
-		}
-		return this._blockColorsMap;
-	}
-	async blockMappings() {
-		if (!this._blockMappings) {
-			this._blockMappings = await fetch(`${URLServerGame(this.local)}/mappings`);
-			this._blockMappings = await this._blockMappings.json();
-		}
-		return this._blockMappings;
-	}
-	async blockManager() {
-		if (!this._blockManager)
-			this._blockManager = await BlockManager.fromClient(this);
-		return this._blockManager;
 	}
 	profiles() {
 		return this.pocketbase.collection("public_profiles");
@@ -95,8 +83,8 @@ export class Client {
 		return await this.pocketbase.send("/api/friends/requests", { token: this.token });
 	}
 	async roomToken(roomId, roomType) {
-		roomType = roomType ?? (this.local ? "pixelwalker_dev" : (await this.roomTypes())[0]);
-		return (await this.pocketbase.send(`/api/joinkey/${roomType}/${roomId}`, { })).token;
+		roomType = roomType ?? (this.local ? "pixelwalker_dev" : this.roomTypes[0]);
+		return (await this.pocketbase.send(`/api/joinkey/${roomType}/${roomId}`, {})).token;
 	}
 }
 
